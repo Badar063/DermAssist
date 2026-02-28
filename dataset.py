5fb2d2f319d0ce3bd1647150065c316548795037
Badar <116815637+Badar063@users.noreply.github.com>
2026-02-28 16:58:26 +0000

    Add SyntheticLesionDataset class for image loading

---
+++ dataset.py
@@ -0,0 +1,23 @@
+import torch
+from torch.utils.data import Dataset
+import pandas as pd
+from PIL import Image
+import os
+
+class SyntheticLesionDataset(Dataset):
+    def __init__(self, csv_file, img_dir, transform=None):
+        self.df = pd.read_csv(csv_file)
+        self.img_dir = img_dir
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.df)
+
+    def __getitem__(self, idx):
+        img_name = self.df.iloc[idx, 0]
+        label = self.df.iloc[idx, 1]
+        img_path = os.path.join(self.img_dir, img_name)
+        image = Image.open(img_path).convert("RGB")
+        if self.transform:
+            image = self.transform(image)
+        return image, label
